35d19ed4276493c2f8fb2e137cd64e83ae9f1de4
Fi H-H <fi.hillier@gmail.com>
2026-04-05 22:04:03 +0100

    Moved data input to sidebar, added cleaning up of date formats to process_csv function, and added validation of date order for grazing data

--- Farm_Data.py
+++ Farm_Data.py
@@ -12,84 +12,78 @@ st.set_page_config(page_title="Farm Data")
 # --- DATA INPUT SECTION ---
 
 # Set up page header
-st.title("🔢 Data Input",text_alignment="center")
-st.divider()
+with st.sidebar:
+    st.title("🔢 Data Input",text_alignment="center")
+    st.divider()
 
-# Define expected headings
-expected_cattle_headings = ["Ear Tag Number", "Date of birth", "M/F", "Bull?", "Date on farm", "Date off farm"]
-expected_field_headings = ["Field Name", "Field Area (Hectare)", "Field Area (Acre)"]
-expected_grazing_headings = ["Your name", "What are the weather conditions?", "Management group", "Date moved out", "Which field are the cattle moving out of?", "What does the paddock the cattle are moving out of look like?", "Date moved in",	
-                            "Which field are the cattle moving into?", "How has the field been split?", "Is this the first, second, third...paddock in the field?", "How many days grazing is intended for this paddock?", "What does the pasture look like?", "What do the cattle look like?"]
-validation_results = []
-#Set up columns
-data_col1, data_col2, data_col3 = st.columns(3)
+    # Define expected headings
+    expected_cattle_headings = ["Ear Tag Number", "Date of birth", "M/F", "Bull?", "Date on farm", "Date off farm"]
+    expected_field_headings = ["Field Name", "Field Area (Hectare)", "Field Area (Acre)"]
+    expected_grazing_headings = ["Your name", "Management group", "Date moved out", "Which field are the cattle moving out of?", "What does the paddock the cattle are moving out of look like?", "Date moved in",	
+                                "Which field are the cattle moving into?", "How has the field been split?", "Is this the first, second, third...paddock in the field?", "What does the pasture look like?", "What do the cattle look like?"]
+    validation_results = []
 
-# Read in cattle data
-with data_col1:
+    # Read in cattle data
     st.subheader("🤠 Cattle Data")
-    st.info(f"**Upload a CSV file of your cattle data. Column headings should be:  \n{expected_cattle_headings}**")
-    cattle_data_table = fn.process_csv("Choose a cattle data CSV file")
-
+    st.info(f"**Upload a CSV file of your cattle data**")
+    cattle_data_table = fn.process_csv("Choose a cattle data CSV file",f"Column headings should be:  \n{expected_cattle_headings}")
     # Check column headings are correct
     if cattle_data_table is not None:
-        validation_results.append(fn.validate_csv_column_headings(cattle_data_table,expected_cattle_headings,data_col1))
+        validation_results.append(fn.validate_csv_column_headings(cattle_data_table,expected_cattle_headings))
 
-# Read in field data
-with data_col2:
+    # Read in field data
     st.subheader("🌿 Field Data")
-    st.info(f"**Upload a CSV file of your field data. Column headings should be:  \n{expected_field_headings}**")
-    field_data_table = fn.process_csv("Choose a field data CSV file")
-
+    st.info(f"**Upload a CSV file of your field data**")
+    field_data_table = fn.process_csv("Choose a field data CSV file",f"Column headings should be:  \n{expected_field_headings}")
     # Check column headings are correct
     if field_data_table is not None:
-        validation_results.append(fn.validate_csv_column_headings(field_data_table,expected_field_headings,data_col2))
+        validation_results.append(fn.validate_csv_column_headings(field_data_table,expected_field_headings))
 
-# Read in grazing data
-with data_col3:
+    # Read in grazing data
     st.subheader("🐮 Grazing Data")
-    st.info("**Upload a CSV file of your grazing data from the google form.  \n Ensure field names match those in the field data csv and entries are in calendar order.**")
-    grazing_data_table = fn.process_csv("Choose a grazing data CSV file")
-    
+    st.info("**Upload a CSV file of your grazing data from the google form**")
+    grazing_data_table = fn.process_csv("Choose a grazing data CSV file","Ensure field names match those in the field data csv and entries are in calendar order - newest at the top to oldest at the bottom")
     # Check column headings are correct
     if grazing_data_table is not None:
-        validation_results.append(fn.validate_csv_column_headings(grazing_data_table,expected_grazing_headings,data_col3))
-
+        validation_results.append(fn.validate_csv_column_headings(grazing_data_table,expected_grazing_headings))
+        validation_results.append(fn.validate_date_order(grazing_data_table,"Date moved out"))
+        validation_results.append(fn.validate_date_order(grazing_data_table,"Date moved in"))
     # Check field names in grazing file matches data in field file
     if grazing_data_table is not None and field_data_table is not None:
         validation_results.append(fn.validate_grazing_data(grazing_data_table, ["Which field are the cattle moving out of?", "Which field are the cattle moving into?"], field_data_table, "Field Name"))
 
-st.divider()
+    st.divider()
 
 # --- DATA ANALYSIS SECTION ---
 
 # Set up page header
-st.title("📊 Data Analysis",text_alignment="center")
+st.title("📊 Grazing Data Analysis",text_alignment="center")
 st.divider()
 
 # Check that there are no errors in the data input before moving on
 if cattle_data_table is not None and field_data_table is not None and grazing_data_table is not None and all(validation_results):
+    st.success("✅ All data present and validated")
     # Add option buttons
     if "livestock_unit" not in st.session_state:
         st.session_state.livestock_unit = False
     if "rest_data" not in st.session_state:
         st.session_state.rest_data = False
-    if "calculate" not in st.session_state:
-        st.session_state.calculate = False
+
     st.subheader("What would you like to calculate?")
     left_space, button_col1, button_col2, right_space = st.columns([2, 2, 2, 2])
     with button_col1:
-        if st.button("Calculate Animal Days per Unit Area", type="primary",width="stretch"):
+        if st.button("🐄 Animal Days per Unit Area", type="primary",width="stretch"):
             st.session_state.livestock_unit = True
             st.session_state.rest_data = False
     with button_col2:
-        if st.button("Calculate Field Rest Period Data",type="primary",width="stretch"):
+        if st.button("🌱 Field Rest Period Data",type="primary",width="stretch"):
             st.session_state.rest_data = True
             st.session_state.livestock_unit = False
     st.divider()
 
     # --- Livestock Unit/Animal Days per Unit Area section ---
     if st.session_state.livestock_unit:
-        st.info("Please enter the livestock units, unit area, and timescale you wish to use for the calculation")
+        st.info("**Please enter the livestock units, unit area, and timescale you wish to use for the calculation**")
 
         # Set up columns
         lu_col1, lu_col2, lu_col3 = st.columns(3)
@@ -106,16 +100,13 @@ if cattle_data_table is not None and field_data_table is not None and grazing_da
 
         with lu_col3:
             # Create date picker to input start and end dates
-            start_date = st.date_input("Start Date", value=dt.date.today(),format="DD/MM/YYYY")
-            end_date = st.date_input("End Date", value=dt.date.today(),format="DD/MM/YYYY")
+            start_date = st.date_input("START DATE", value=dt.date.today(),format="DD/MM/YYYY")
+            end_date = st.date_input("END DATE", value=dt.date.today(),format="DD/MM/YYYY")
 
         st.divider()
 
-        # Set calculate button
-        if st.button("Calculate", type="primary",width="content"):
-            st.session_state.calculate = True
-        if st.session_state.calculate:
-            # Calculate animal groups
+        # Calculate animal groups
+        if lu_input_parameters is not None and area_unit is not None and start_date is not None and end_date is not None:
             animal_days_per_unit_area = fn.calculate_animal_days_per_area_fast(grazing_data_table,cattle_data_table,field_data_table,area_unit, lu_input_parameters["VALUE"])
             st.dataframe(data=animal_days_per_unit_area, width="content",hide_index=True,key="animal_days_per_unit_area_table")
 
@@ -123,8 +114,11 @@ if cattle_data_table is not None and field_data_table is not None and grazing_da
     if st.session_state.rest_data:
         st.warning("The field rest data feature has not yet been implemented ☹️")
 
+elif cattle_data_table is not None and field_data_table is not None and grazing_data_table is not None:
+    st.error("❌ Please fix identified errors in data input.")
+
 else:
-        st.error("❌ Data input errors must be fixed before continuing")
+    st.warning("⚠️ Please select cattle data, field data, and grazing data files to be used.")
 
 
 
--- Functions.py
+++ Functions.py
@@ -20,19 +20,34 @@ def load_csv(file):
     # Reads in csv. This only runs once per file to save time
     return pd.read_csv(file)
 
-def process_csv(label):
+def ensure_datetime_cols(dataframe, columns_to_check):
+    # Create a copy so we don't accidentally modify the original unexpectedly
+    copied_dataframe = dataframe.copy()
+    
+    for col in columns_to_check:
+        if col in copied_dataframe.columns:
+            # errors='coerce' turns unparseable dates into NaT (Not a Time)
+            copied_dataframe[col] = pd.to_datetime(copied_dataframe[col], errors='coerce', dayfirst=True, format="mixed")
+            
+    return copied_dataframe
+
+def process_csv(label,help_text):
     # Create the file uploader widget
-    uploaded_file = st.file_uploader(label, type="csv",key=label)
+    uploaded_file = st.file_uploader(label, type="csv",key=label,help=help_text)
 
     if uploaded_file is not None:
         # Read the file into a Pandas DataFrame
         df = load_csv(uploaded_file)
-        return df
+        # Convert any date columns to datetime
+        date_columns = ["Date of birth", "Date on farm", "Date off farm", "Date moved out", "Date moved in"]
+        cleaned_df = ensure_datetime_cols(df,date_columns)
+        
+        return cleaned_df
     else:
-        st.error("💡 Please upload a CSV file to get started.")
+        st.warning("💡 Please upload a CSV file to get started.")
         return None
 
-def validate_csv_column_headings(data_table,expected_headings, col_container):
+def validate_csv_column_headings(data_table,expected_headings):
         actual_cols = set(data_table.columns)
         required_cols = set(expected_headings)
         
@@ -40,11 +55,11 @@ def validate_csv_column_headings(data_table,expected_headings, col_container):
         missing_cols = required_cols - actual_cols
         
         if not missing_cols:
-            col_container.success(f"✅ csv columns validated!")
+            st.success(f"✅ csv columns validated!")
             return True
         else:
             # Display a clear error message
-            col_container.error(f"❌ Required column missing in csv. Missing: {', '.join(missing_cols)}")
+            st.error(f"❌ Required column missing in csv. Missing: {', '.join(missing_cols)}")
             return False
 
 def validate_grazing_data(grazing_table, grazing_col_name, field_table, field_col_name):
@@ -72,6 +87,19 @@ def validate_grazing_data(grazing_table, grazing_col_name, field_table, field_co
         st.dataframe(error_rows, width='stretch')
         return False
     
+def validate_date_order(data_table,col_name):
+    is_ordered = data_table[col_name].is_monotonic_decreasing
+    is_error = data_table[col_name].diff() > pd.Timedelta(0)
+    error_rows = data_table[is_error]
+
+    if not error_rows.empty:
+        st.error(f"❌ Entries in **{col_name}** column are not in order from newest to oldest")
+        st.dataframe(error_rows[col_name])
+        return False
+    else:
+        st.success(f"✅ Entries in **{col_name}** column are in correct order")
+        return True
+
 def create_input_table(template_data,table_index,index_name,table_key):
     # Create pandas dataframe with template data
     template_table = pd.DataFrame(template_data,index=table_index)
@@ -131,12 +159,6 @@ def calculate_animal_groups(calc_date,cattle_data,lu_data):
     return active.groupby("Group")["Livestock Unit"].sum().to_dict()
 
 def calculate_animal_days_per_area_fast(grazing_data_table, cattle_data_table, field_table, field_area_unit, lu_data):
-    # Pre-process dates
-    grazing_data_table['Date moved out'] = pd.to_datetime(grazing_data_table['Date moved out'], dayfirst=True,format='%d-%b-%y')
-    grazing_data_table['Date moved in'] = pd.to_datetime(grazing_data_table['Date moved in'], dayfirst=True,format='%d-%b-%y')
-    cattle_data_table['Date of birth'] = pd.to_datetime(cattle_data_table['Date of birth'], dayfirst=True)
-    cattle_data_table['Date on farm'] = pd.to_datetime(cattle_data_table['Date on farm'], dayfirst=True)
-    cattle_data_table['Date off farm'] = pd.to_datetime(cattle_data_table['Date off farm'], dayfirst=True)
 
     # Field Area lookup
     area_col = 'Field Area (Hectare)' if field_area_unit == "Hectare" else 'Field Area (Acre)'
@@ -203,12 +225,12 @@ def calculate_animal_days_per_area_fast(grazing_data_table, cattle_data_table, f
                 animal_days = round((total_lu * days_in_field) / effective_area, 1)
                 
         results.append({
-            'Field': field_name,
-            'Date': out_date_str,
-            'Total Livestock Units': total_lu,
-            'Paddock Area': effective_area,
-            'Grazing Period': days_in_field,
-            'AnimalDays/Area': animal_days
+            'FIELD': field_name,
+            'DATE': out_date_str,
+            'TOTAL LIVESTOCK UNITS': total_lu,
+            'PADDOCK AREA': effective_area,
+            'GRAZING PERIOD': days_in_field,
+            f'ANIMAL DAYS/{str(field_area_unit).upper()}': animal_days
         })
 
     return pd.DataFrame(results)
